c230b69018e4094e543993a5183d6aee2429c28c
John L. Singleton <jsinglet@github.com>
2023-09-07 18:46:17 -0400

    refactor structure

---
+++ javascript/javascript-sap-ui5-queries/test/UI5LogInjection/xss-custom-control-property-sanitized/webapp/control/xss.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/Control"
+], function (Control) {
+    return Control.extend("codeql-sap-js.control.xss", {
+        metadata: {
+            properties: {
+                text: { type: "int" }
+            }
+        },
+        renderer: {
+            apiVersion: 2,
+            render: function (oRm, oControl) {
+                oRm.openStart("div", oControl);
+                oRm.unsafeHtml(oControl.getText()); // XSS sink sanitized
+                oRm.close("div");
+                jQuery.sap.log.debug(oControl.getText()); //log-injection sink non-sanitized
+            }
+        }
+    });
+})
