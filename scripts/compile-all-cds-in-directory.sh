2230f11721999b656b2f0fce54a6d9eceb8366ef
Jeongsoo Lee <jeongsoolee09@github.com>
2025-06-17 14:43:42 -0400

    Don't install globally and use `npx cds`
    It's not desirable to mess with the $PATH
    directories. Instead, do it per-project and use
    the `npx` command to invoke the binary.

--- scripts/compile-all-cds-in-directory.sh
+++ scripts/compile-all-cds-in-directory.sh
@@ -2,15 +2,12 @@
 # !!!!!!! Run it at the root of a CAP application, where its package.json is !!!!!!!
 
 # Ensure that we have the `cds` command
-if ! command -v cds &> /dev/null
-then
-    npm install -g @sap/cds
-fi
+npm install @sap/cds
 
 # Compile all .cds files to .json
 for cds_file in $(find . -type f \( -iname '*.cds' \) -print)
 do
-    cds compile $cds_file \
+    npx cds compile $cds_file \
         -2 json \
         -o "$cds_file.json" \
         --locations
