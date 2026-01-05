4be732a5d468e8d572808360bea127d16f63530b
Kristen Newbury <knewbury01@github.com>
2026-01-05 13:35:03 -0500

    Fix syntax error in testcases for fragments

--- javascript/frameworks/ui5/test/queries/UI5Xss/xss-xml-fragment-load/webapp/controller/app.controller.js
+++ javascript/frameworks/ui5/test/queries/UI5Xss/xss-xml-fragment-load/webapp/controller/app.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/Fragment"
 ], function (Controller, JSONModel, Fragment) {
-    "use strict"
+    "use strict";
     return Controller.extend("codeql-sap-js.controller.app", {
         onInit: function () {
             var oData = {
