b41d58d3d27c053a46db0a6d38cc4cdfa7a9356d
Nathan Randall <data-douser@github.com>
2026-01-29 12:00:30 -0700

    Create external_model_manifest UI5 models unit test
    This commit:
    
    - Creates a new models test directory under:
      `javascript/frameworks/ui5/test/modles/external_model_manifest/`
    
    - Address PR review feedback from PR #276 :
      https://github.com/advanced-security/codeql-sap-js/pull/276#discussion_r2683614608
    
    - Adds missing .expected files, where needed, for existing UI5
      query unit tests -- which fixes "strict" test discovery for
      "UI5LogInjection" query unit tests.
    
    This new `external_model_manifest` UI5 lib unit test:
    
    - **Tests ExternalModelManifest class**
      validates the modeling of external models that reference data
      sources in manifest.json
    
    - **Tests ODataDataSourceManifest class**
      specifically validates OData data source modeling

---
+++ javascript/frameworks/ui5/test/models/external_model_manifest/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], function(Controller) {
+  "use strict";
+  return Controller.extend("external.model.manifest.test.controller.App", {
+    onInit: function() {
+      // Simple controller for testing
+    }
+  });
+});
