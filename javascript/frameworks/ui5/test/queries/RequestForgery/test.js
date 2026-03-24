5f607c51af87134b6692ef83973f56651f9c04ef
Mauro Baluda <mbaluda@github.com>
2026-03-24 09:36:42 +0100

    CSRF test

--- javascript/frameworks/ui5/test/queries/RequestForgery/test.js
+++ javascript/frameworks/ui5/test/queries/RequestForgery/test.js
@@ -1,6 +1,2 @@
-import request from 'request';
-$(document).ready(function () {
-    var request = new XMLHttpRequest();
-    var url = jQuery.sap.GetUriParameters().get("url");
-    request.open("GET", url, false);
-});
+var url = jQuery.sap.getUriParameters().get("url");
+new XMLHttpRequest().open("GET", url, false);
